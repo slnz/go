2b767679f54f2737aae72ba070c716253ea69b2d
Ziwei Liu <ziwei.liu.23@gmail.com>
2022-07-13 11:54:53 +1200

    feat: add get process defintions

--- src/@types/fluro.d.ts
+++ src/@types/fluro.d.ts
@@ -4,8 +4,6 @@
 declare module 'fluro' {
   export type DefinitionName = 'contact' | 'process'
 
-  export type Types = Process
-
   export interface Definition {
     definitionName: string
     definitions: ContentDefinition[]
@@ -124,7 +122,7 @@ declare module 'fluro' {
 
     types: {
       /**
-       * Retrieves a list of specified types and their respective definitions
+       * Retrieves a list of specified types and their respective definitions. Get method does not return data property on ContentDefinition - must use retrieve.
        */
       retrieve: (array: DefinitionName[]) => Promise<Definition[]>
     }
---
+++ src/lib/queries/getProcessDefinitions.ts
@@ -0,0 +1,20 @@
+import { ContentDefinition, ProcessDefinitionData } from 'fluro'
+
+import { client } from '../fluro'
+
+export interface GetProcessDefinitions {
+  [definitionName: string]: ContentDefinition<ProcessDefinitionData>
+}
+
+export async function getProcessDefinitions(): Promise<GetProcessDefinitions> {
+  return client.types.retrieve(['process']).then((data) => {
+    console.log('data', data)
+    const definitions: {
+      [key: string]: ContentDefinition<ProcessDefinitionData>
+    } = {}
+    data[0].definitions.forEach((definition): void => {
+      definitions[definition.definitionName] = definition
+    })
+    return definitions
+  })
+}
